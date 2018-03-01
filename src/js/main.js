a8ad61b677b0e93232958b838641a7f74b59c635
Allyson Souza <allyson@hastedesign.com.br>
2018-03-01 13:05:17 -0300

    Removed unecessary code from main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,31 +1,3 @@
 /**
  *	Main
  */
-jQuery(document).ready(function($) {
-
-	/**
-	 * Responsive wp_video_shortcode().
-	 */
-
-	$( '.wp-video-shortcode' ).parent( 'div' ).css( 'width', 'auto' );
-
-	/**
-	 * Fluid width video embeds.
-	 * @link http://fitvidsjs.com
-	 */
-
-	$( '.entry-content' ).fitVids();
-
-	/**
-	 * Odin Core shortcodes.
-	 */
-
-	// Tabs.
-	//$( '.odin-tabs a' ).click(function(e) {
-	//	e.preventDefault();
-	//	$(this).tab( 'show' );
-	//});
-
-	// Tooltip.
-	//$( '.odin-tooltip' ).tooltip();
-});
