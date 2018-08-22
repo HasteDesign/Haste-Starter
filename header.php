89e9779d20d909f11e11f4535299f73b273c9ea0
Allyson Souza <allyson@hastedesign.com.br>
2018-08-22 01:50:54 -0300

    Fixing babel task - Babel will only compile src/js/main.js to assets/js/main.js - assets/js/main.js will be uglified with jQuery and Bootstrap after

--- header.php
+++ header.php
@@ -31,6 +31,8 @@
 		<?php get_template_part( 'template-parts/header', 'navigation' ); ?>
 
 		<?php get_template_part( 'template-parts/header', 'image' ); ?>
+
+		<div class="dropdown">
 	</header><!-- #header -->
 
 	<div id="wrapper" class="container">
--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,11 @@
 /**
  * Main
+ *
+ * You can write latest EcmaScript version in this file and it'll be
+ * transpiled with Babel on npm run babel or npm run build:js.
+ * More information in documentation: https://github.com/HasteDesign/Haste-Starter/wiki
  */
 
+/* ES6 Example code */
+// let a = 40;
+// console.log(a);
