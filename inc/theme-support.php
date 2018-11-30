4a789a8324ec15acd7dc5eb24d17b921f187c4cf
Allyson Souza <allyson@hastedesign.com.br>
2018-11-30 20:15:27 -0200

    Starting support to Gutenberg - Created haste_starter_gutenberg_support function - Created some scaffolding for theme development and Gutenberg supports and opt-ins

--- inc/theme-support.php
+++ inc/theme-support.php
@@ -1,6 +1,6 @@
 <?php
 
-if ( ! function_exists( 'haste_starter_theme_support' ) ) :
+if ( ! function_exists( 'haste_starter_theme_support' ) ) {
 	/**
 	 * Setup theme support.
 	 *
@@ -90,11 +90,11 @@ if ( ! function_exists( 'haste_starter_theme_support' ) ) :
 		) );
 
 	} /* end theme support */
-endif;
+}
 
 add_action( 'after_setup_theme', 'haste_starter_theme_support' );
 
-if ( ! function_exists( 'haste_starter_content_width' ) ) :
+if ( ! function_exists( 'haste_starter_content_width' ) ) {
 	/**
 	 * Sets the content width in pixels, based on the theme's design and stylesheet.
 	 *
@@ -105,6 +105,96 @@ if ( ! function_exists( 'haste_starter_content_width' ) ) :
 	function haste_starter_content_width() {
 		$GLOBALS['content_width'] = apply_filters( 'haste_starter_content_width', 860 );
 	}
-endif;
+}
 
 add_action( 'after_setup_theme', 'haste_starter_content_width', 0 );
+
+if ( ! function_exists( 'haste_starter_gutenberg_support' ) ) {
+	/**
+	 * Setup theme support.
+	 *
+	 * @link https://codex.wordpress.org/Function_Reference/add_theme_support
+	 */
+	function haste_starter_gutenberg_support() {
+
+		// Editor color palette
+		// You can disable or provide your customn collor palette, choose the one you want for your theme.
+		// https://wordpress.org/gutenberg/handbook/designers-developers/developers/themes/theme-support/#block-color-palettes
+
+		//add_theme_support( 'disable-custom-colors' );
+
+		add_theme_support( 'editor-color-palette', array(
+			array(
+				'name' => __( 'haste starter cyan', 'haste-starter' ),
+				'slug' => 'haste-starter-cyan',
+				'color' => '#00C9C8',
+			),
+			array(
+				'name' => __( 'haste starter violet', 'haste-starter' ),
+				'slug' => 'haste-starter-violet',
+				'color' => '#6428FF',
+			),
+			array(
+				'name' => __( 'haste starter red', 'haste-starter' ),
+				'slug' => 'haste-starter-red',
+				'color' => '#FF2C18',
+			),
+			array(
+				'name' => __( 'haste starter light gray', 'haste-starter' ),
+				'slug' => 'haste-starter-light-gray',
+				'color' => '#FAFAFA',
+			),
+		) );
+
+		// Wide alignment
+		// https://wordpress.org/gutenberg/handbook/designers-developers/developers/themes/theme-support/#wide-alignment
+		// add_theme_support( 'align-wide' );
+
+		// Font sizes
+		// You can disable or provide your customn font sizes, choose the one you want for your theme.
+		// https://wordpress.org/gutenberg/handbook/designers-developers/developers/themes/theme-support/#block-font-sizes
+
+		// add_theme_support('disable-custom-font-sizes');
+
+		add_theme_support( 'editor-font-sizes', array(
+			array(
+				'name' => __( 'small', 'haste-starter' ),
+				'shortName' => __( 'S', 'haste-starter' ),
+				'size' => 12,
+				'slug' => 'small'
+			),
+			array(
+				'name' => __( 'regular', 'haste-starter' ),
+				'shortName' => __( 'M', 'haste-starter' ),
+				'size' => 16,
+				'slug' => 'regular'
+			),
+			array(
+				'name' => __( 'large', 'haste-starter' ),
+				'shortName' => __( 'L', 'haste-starter' ),
+				'size' => 36,
+				'slug' => 'large'
+			),
+			array(
+				'name' => __( 'larger', 'haste-starter' ),
+				'shortName' => __( 'XL', 'haste-starter' ),
+				'size' => 50,
+				'slug' => 'larger'
+			)
+		) );
+
+		// Editor styles
+		// https://wordpress.org/gutenberg/handbook/designers-developers/developers/themes/theme-support/#dark-backgrounds
+		//add_theme_support( 'editor-styles' );
+		//add_theme_support( 'dark-editor-style' );
+
+		// WP block styles
+		//add_theme_support( 'wp-block-styles' );
+
+		// Responsive embeds
+		// https://wordpress.org/gutenberg/handbook/designers-developers/developers/themes/theme-support/#responsive-embedded-content
+		add_theme_support( 'responsive-embeds' );
+	}
+}
+
+add_action( 'after_setup_theme', 'haste_starter_gutenberg_support' );
