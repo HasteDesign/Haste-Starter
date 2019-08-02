7899e45225a6be87840a44c6fd1523a1009cb9d6
Allyson Souza <allyson@hastedesign.com.br>
2019-08-02 13:12:13 -0300

    Fixed require of walker and widget classes in functions.php

--- functions.php
+++ functions.php
@@ -22,14 +22,14 @@ if ( file_exists( get_template_directory() . '/vendor/autoload.php' ) ) {
 /**
  * Classes.
  */
-require_once get_template_directory() . '/inc/classes/class-bootstrap-nav.php';
+require_once get_template_directory() . '/inc/classes/class-haste-starter-bootstrap-nav-walker.php';
 
 /**
  * Widgets.
  */
 
 // Haste Widget
-// require_once get_template_directory() . '/inc/widgets/class-widget-haste.php';
+// require_once get_template_directory() . '/inc/widgets/class-haste-starter-widget.php';
 
 /**
  * Hooks.
