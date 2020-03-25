29ed4f7706fa4ba5299316a8cf7a4d1335e95063
GabrielHenriqueRS <gabriel@hastedesign.com.br>
2020-03-25 19:48:35 -0300

    Fixes #37 - Footer: review the colophon info

--- footer.php
+++ footer.php
@@ -12,9 +12,11 @@
 ?>
 
 	<footer id="footer" role="contentinfo">
-		<div class="container">
-			<p>&copy; <?php echo date( 'Y' ); ?> <a href="<?php echo esc_url( home_url() ); ?>"><?php bloginfo( 'name' ); ?></a> -
-				<?php _e( 'All rights reserved', 'haste-starter' ); ?> | <?php echo sprintf( __( 'Powered by the <a href="%1$s" rel="nofollow" target="_blank">Haste Starter</a> forces and <a href="%2$s" rel="nofollow" target="_blank">WordPress</a>.', 'haste-starter' ), 'https://github.com/HasteDesign/Haste-Starter', 'http://wordpress.org/' ); ?></p>
+		<div class="colophon">
+			<div class="container">
+				<p>&copy; <?php echo date( 'Y' ); ?> <a href="<?php echo esc_url( home_url() ); ?>"><?php bloginfo( 'name' ); ?></a></p>
+				<a title="Site desenvolvido por Estúdio Haste" href="http://www.hastedesign.com.br">Desenvolvido por Estúdio Haste</a>
+			</div>
 		</div>
 	</footer>
 
