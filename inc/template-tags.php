9fdcb5c48e1035ace2c10b38ae0b5b793e0e8896
Yan Kairalla <yanmkairalla@hotmail.com>
2021-08-20 19:22:54 -0300

    Update custom logo, criado function para nome e image

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -163,15 +163,16 @@ if ( ! function_exists( 'haste_starter_the_custom_logo' ) ) {
 			$custom_logo_id = get_theme_mod( 'custom_logo' );
 			$logo           = wp_get_attachment_image_src( $custom_logo_id, 'full' );
 
-			if ( has_custom_logo() ) {
-
-					echo '<img class="site-logo" alt="Logo ' . esc_attr( get_bloginfo( 'name', 'display' ) ) . '" src="' . esc_url( $logo[0] ) . '">';
-
-			} else {
-				echo haste_starter_custom_logo_name();
-			}
+			echo has_custom_logo() ? haste_starter_custom_logo_image( $logo ) : haste_starter_custom_logo_name();
 		}
 		echo '</a>';
+	}
+}
+
+if ( ! function_exists( 'haste_starter_custom_logo_image' ) ) {
+	function haste_starter_custom_logo_image( $logo ) {
+		return '<img class="site-logo" alt="Logo ' . esc_attr( get_bloginfo( 'name', 'display' ) ) . '" src="' . esc_url( $logo[0] ) . '">';
+
 	}
 }
 
