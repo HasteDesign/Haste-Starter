272a610ee820d66533ff8919112f1789dc76df5f
Yan Kairalla <yanmkairalla@hotmail.com>
2021-08-24 20:58:49 -0300

    removed tgmpa from composer and files

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -13,6 +13,9 @@ class ComposerAutoloaderInit5354725ad53c4ef9bb82ff9abede7f18
         }
     }
 
+    /**
+     * @return \Composer\Autoload\ClassLoader
+     */
     public static function getLoader()
     {
         if (null !== self::$loader) {
@@ -47,24 +50,6 @@ class ComposerAutoloaderInit5354725ad53c4ef9bb82ff9abede7f18
 
         $loader->register(true);
 
-        if ($useStaticLoader) {
-            $includeFiles = Composer\Autoload\ComposerStaticInit5354725ad53c4ef9bb82ff9abede7f18::$files;
-        } else {
-            $includeFiles = require __DIR__ . '/autoload_files.php';
-        }
-        foreach ($includeFiles as $fileIdentifier => $file) {
-            composerRequire5354725ad53c4ef9bb82ff9abede7f18($fileIdentifier, $file);
-        }
-
         return $loader;
-    }
-}
-
-function composerRequire5354725ad53c4ef9bb82ff9abede7f18($fileIdentifier, $file)
-{
-    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
-        require $file;
-
-        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
     }
 }
--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -6,10 +6,6 @@ namespace Composer\Autoload;
 
 class ComposerStaticInit5354725ad53c4ef9bb82ff9abede7f18
 {
-    public static $files = array (
-        'a5f882d89ab791a139cd2d37e50cdd80' => __DIR__ . '/..' . '/tgmpa/tgm-plugin-activation/class-tgm-plugin-activation.php',
-    );
-
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
