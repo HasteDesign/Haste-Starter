5903c29558d3baa8b19551a27719a4d3ce91592d
Allyson Souza <allyson@hastedesign.com.br>
2021-08-28 14:37:53 -0300

    Removed old widget cleanup

--- inc/cleanup.php
+++ inc/cleanup.php
@@ -7,13 +7,7 @@
  * Cleanup wp_head().
  */
 function haste_starter_head_cleanup() {
-	// Category feeds.
-	// remove_action( 'wp_head', 'feed_links_extra', 3 );
-
-	// Post and comment feeds.
-	// remove_action( 'wp_head', 'feed_links', 2 );
-
-	// EditURI link.
+	// Edit URI link.
 	remove_action( 'wp_head', 'rsd_link' );
 
 	// Windows live writer.
@@ -35,66 +29,17 @@ function haste_starter_head_cleanup() {
 	remove_action( 'wp_head', 'wp_generator' );
 
 	// Emoji's
-	// remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
-	// remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
-	// remove_action( 'wp_print_styles', 'print_emoji_styles' );
-	// remove_action( 'admin_print_styles', 'print_emoji_styles' );
-	// remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
-	// remove_filter( 'comment_text_rss', 'wp_staticize_emoji' );
-	// remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
+	remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+	remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+	remove_action( 'wp_print_styles', 'print_emoji_styles' );
+	remove_action( 'admin_print_styles', 'print_emoji_styles' );
+	remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
+	remove_filter( 'comment_text_rss', 'wp_staticize_emoji' );
+	remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
 }
-
 add_action( 'init', 'haste_starter_head_cleanup' );
 
 /**
  * Remove WP version from RSS.
  */
 add_filter( 'the_generator', '__return_false' );
-
-/**
- * Remove injected CSS for recent comments widget.
- */
-function haste_starter_remove_wp_widget_recent_comments_style() {
-	if ( has_filter( 'wp_head', 'wp_widget_recent_comments_style' ) ) {
-		remove_filter( 'wp_head', 'wp_widget_recent_comments_style' );
-	}
-}
-add_filter( 'wp_head', 'haste_starter_remove_wp_widget_recent_comments_style', 1 );
-
-/**
- * Remove injected CSS from recent comments widget.
- */
-function haste_starter_remove_recent_comments_style() {
-	global $wp_widget_factory;
-
-	if ( isset( $wp_widget_factory->widgets['WP_Widget_Recent_Comments'] ) ) {
-		remove_action( 'wp_head', array( $wp_widget_factory->widgets['WP_Widget_Recent_Comments'], 'recent_comments_style' ) );
-	}
-}
-add_action( 'wp_head', 'haste_starter_remove_recent_comments_style', 1 );
-
-/**
- * Remove injected CSS from gallery.
- */
-add_filter( 'use_default_gallery_style', '__return_false' );
-
-/**
- * Add rel="nofollow" and remove rel="category".
- */
-function haste_starter_modify_category_rel( $text ) {
-	$search = array( 'rel="category"', 'rel="category tag"' );
-	$text   = str_replace( $search, 'rel="nofollow"', $text );
-
-	return $text;
-}
-add_filter( 'wp_list_categories', 'haste_starter_modify_category_rel' );
-add_filter( 'the_category', 'haste_starter_modify_category_rel' );
-
-/**
- * Add rel="nofollow" and remove rel="tag".
- */
-function haste_starter_modify_tag_rel( $taglink ) {
-	return str_replace( 'rel="tag">', 'rel="nofollow">', $taglink );
-}
-add_filter( 'wp_tag_cloud', 'haste_starter_modify_tag_rel' );
-add_filter( 'the_tags', 'haste_starter_modify_tag_rel' );
